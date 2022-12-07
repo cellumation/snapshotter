21935a822f538ab2192aff210fec96812969f27e
Alexis Tsogias <a.tsogias@cellumation.com>
2022-12-07 17:41:32 +0100

    feat: add an "include_topics" list that allows to include topics that would otherwise be excluded

--- src/TopicFilter.cpp
+++ src/TopicFilter.cpp
@@ -1,60 +1,73 @@
 /*********************************************************************
-* Software License Agreement (BSD License)
-*
-*  Copyright (c) 2021, Cellumation GmbH
-*  All rights reserved.
-*
-*  Redistribution and use in source and binary forms, with or without
-*  modification, are permitted provided that the following conditions
-*  are met:
-*
-*   * Redistributions of source code must retain the above copyright
-*     notice, this list of conditions and the following disclaimer.
-*   * Redistributions in binary form must reproduce the above
-*     copyright notice, this list of conditions and the following
-*     disclaimer in the documentation and/or other materials provided
-*     with the distribution.
-*   * Neither the name of Willow Garage, Inc. nor the names of its
-*     contributors may be used to endorse or promote products derived
-*     from this software without specific prior written permission.
-*
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-*  POSSIBILITY OF SUCH DAMAGE.
-********************************************************************/
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2021, Cellumation GmbH
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ ********************************************************************/
+
 #include "TopicFilter.hpp"
 #include <ros/console.h>
+
 namespace snapshotter
 {
-
-TopicFilter::TopicFilter(const TopicFilterConfig& cfg)
+TopicFilter::TopicFilter(const std::vector<std::string>& excludeRegexps_,
+                         const std::vector<std::string>& includeRegexps_)
 {
-    for(const std::string& regexp : cfg.excludeRegexps)
+    for (const std::string& regexp : excludeRegexps_)
     {
         excludeRegexps.emplace_back(regexp);
         ROS_INFO_STREAM("Excluding topic: " << regexp);
+    }
+    for (const std::string& regexp : includeRegexps_)
+    {
+        includeRegexps.emplace_back(regexp);
+        ROS_INFO_STREAM("Including topic: " << regexp);
     }
 }
 
 bool TopicFilter::exclude(const std::string& topic) const
 {
-    for(const boost::regex& r : excludeRegexps)
+    for (const boost::regex& er : excludeRegexps)
     {
-        if(boost::regex_match(topic, r))
+        if (boost::regex_match(topic, er))
         {
+            for (const boost::regex& ir : includeRegexps)
+            {
+                if (boost::regex_match(topic, ir))
+                {
+                    return false;
+                }
+            }
             return true;
         }
     }
     return false;
 }
-
-}//end namespace
+} // namespace snapshotter
